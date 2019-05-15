56e23932c306481e28c2a1f865ffee85a80b8aa4
ChrstphGrnr <ChristophGruener@me.com>
2019-05-15 10:54:39 -0600

    add CRUD actions

--- app/controllers/r_vs_controller.rb
+++ app/controllers/r_vs_controller.rb
@@ -1,2 +1,26 @@
 class RVsController < ApplicationController
+
+    def new 
+    end
+
+    def create
+    end
+
+    def edit 
+    end
+    
+    def index
+    end
+
+    def show 
+    end
+
+    def destroy
+    end
+
+    private 
+    
+        def rv_params
+            params.require(:rv).permit(:name, :owner_id, :sleeps, :price, :trip_count)
+        end
 end
