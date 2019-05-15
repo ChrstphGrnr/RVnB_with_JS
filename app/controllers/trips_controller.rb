d573e71dc5993ac9010bbd50bb19652d03ede6a3
ChrstphGrnr <ChristophGruener@me.com>
2019-05-15 16:41:50 -0600

    change new, create and show action, create strong_params

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,28 @@
 class TripsController < ApplicationController
 
     def new
+        # byebug
+        if params[:rv_id]
+            @trip = Trip.new(rv_id: params[:rv_id])
+        else 
+            @trip = Trip.new
+        end
+                
     end
 
     def create
+        # byebug
+        @trip = Trip.new(trip_params)
+        if @trip.save 
+            redirect_to trip_path(@trip)
+        else
+            render 'trips/new', alert: "Invalid Data, please try again."
+        end
     end
     
 
     def show 
+        @trip = Trip.find(params[:id])
     end
 
     def index
@@ -19,4 +34,9 @@ class TripsController < ApplicationController
     def destroy
     end
 
+    private 
+
+        def trip_params
+            params.require(:trip).permit(:name, :start_date, :end_date, :guests, :user_id, :rv_id)
+        end
 end
