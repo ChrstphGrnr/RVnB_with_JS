c551da78a894cda88eac24b3342d37de199b2628
ChrstphGrnr <ChristophGruener@me.com>
2019-05-15 16:51:51 -0600

    create RVs controller with actions and strong_params

---
+++ app/controllers/rvs_controller.rb
@@ -0,0 +1,28 @@
+class RvsController < ApplicationController
+
+    def new 
+    end
+
+    def create
+    end
+
+    def edit 
+    end
+    
+    def index
+        @rvs = Rv.all
+    end
+
+    def show 
+        @rv = Rv.find(params[:id])
+    end
+
+    def destroy
+    end
+
+    private 
+    
+        def rv_params
+            params.require(:rv).permit(:name, :owner_id, :sleeps, :price, :trip_count)
+        end
+end
