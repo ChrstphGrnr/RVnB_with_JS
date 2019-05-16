93bf5f2454c7deeaf715ec92d5443f9c9ad339e2
ChrstphGrnr <ChristophGruener@me.com>
2019-05-16 13:59:00 -0600

    implement omniauth-github for users login

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
   
   resources :trips, only: [:new, :edit, :index, :show, :create]
   
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations' }, :controllers => {omniauth_callbacks: "callbacks"}
+  resources :users do 
+    resources :trips, only: [:index]    
+  end
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#welcome'
