54511881d3663d80138c893e8e174ed1388e3a90
ChrstphGrnr <ChristophGruener@me.com>
2019-05-17 19:14:16 -0600

    add class level methods for ActiveRecord callbacks

--- app/models/rv.rb
+++ app/models/rv.rb
@@ -1,3 +1,12 @@
 class Rv < ApplicationRecord
     has_many :users, through: :trips
+
+    def self.highest_trip_count
+        order("trip_count DESC").first
+    end
+
+    def self.order_by_price
+        order("price ASC")
+    end
+    
 end
