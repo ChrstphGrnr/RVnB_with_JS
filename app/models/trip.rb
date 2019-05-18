ccdde63f1ae955ccd0ab9e864d6cf7c33d76fda7
ChrstphGrnr <ChristophGruener@me.com>
2019-05-17 20:13:37 -0600

    add total price and number of days instance methods

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,4 +5,14 @@ class Trip < ApplicationRecord
     def self.trips_by_user(user_id) 
         where(user_id: user_id)
     end
+
+    def total_price
+        self.rv.price.remove(/\D/).to_i*number_of_days        
+    end 
+
+    def number_of_days
+        (self.end_date.to_datetime - self.start_date.to_datetime).to_i
+    end
+
+
 end
