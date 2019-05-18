708510882f088b117d4d11c28766cd89e5524963
ChrstphGrnr <ChristophGruener@me.com>
2019-05-17 20:12:42 -0600

    add total price and number of days instance methods

--- app/models/rv.rb
+++ app/models/rv.rb
@@ -8,5 +8,14 @@ class Rv < ApplicationRecord
     def self.order_by_price
         order("price ASC")
     end
+
+    def total_price
+        self.price*number_of_days        
+    end 
+
+    def number_of_days
+        days = self.end_date - self.start_date
+    end
+
     
 end
